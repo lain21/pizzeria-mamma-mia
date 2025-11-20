ae8472a0c97ff63366f68d7beb04db8c672fe1b9
lain21 <abraham.m.s@outlook.com>
2025-11-19 21:33:16 -0300

    Hito 3 completado: Home, CardPizza, Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css';
+import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+// import LoginPage from "./components/Login";
+// import RegisterPage from "./components/Register";
+// import Cart from "./components/Cart";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      {/* <Home /> */} 
+      <Cart />
+      {/* <RegisterPage /> */}
+      {/* <LoginPage /> */}
+      {/* <Cart /> */}
       <Footer />
     </>
   );
