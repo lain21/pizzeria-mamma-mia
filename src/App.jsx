4a64ca590c4500d02e4f0f7b58dbba36d959c2a3
lain21 <abraham.m.s@outlook.com>
2025-11-20 21:48:06 -0300

    Hito 4 funcionando: integración con backend, componentes corregidos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+// import Login from "./components/Login";  // <-- QUITAR
+import RegisterPage from "./components/RegisterPage"; // <-- ESTE SÍ EXISTE
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Home />
+      {/* <Login /> */}   {/* <-- QUITAR DEL JSX TAMBIÉN */}
       <Footer />
     </>
   );
